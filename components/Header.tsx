8fb3c315511e2fcbd936fea7be548b4f3f2f6d90
송상록 <drinkingstraw33@gmail.com>
2025-10-08 00:11:42 +0900

    fix: 인쇄용 CSS 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
 
 const Header = () => {
-  let headerClass = 'flex items-center w-full justify-between py-10'
+  let headerClass = 'flex items-center w-full justify-between py-10 print:hidden'
   if (siteMetadata.stickyNav) {
     headerClass += ' sticky top-0 z-50'
   }
