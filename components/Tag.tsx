38c33c905fe5f17379c64a877bcaceb3f956e7f4
송상록 <drinkingstraw33@gmail.com>
2025-10-08 00:42:10 +0900

    fix: tag 디자인 원복

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const Tag = ({ text }: Props) => {
   return (
-    <span className="bg-primary-100 text-primary-700 dark:bg-primary-900 dark:text-primary-300 mr-2 inline-block rounded-full border px-3 py-1 text-sm font-medium whitespace-nowrap print:px-1.5 print:py-0.5 print:text-xs">
+    <span className="border-primary-500 text-primary-500 dark:border-primary-400 dark:text-primary-400 mr-2 inline-block rounded-full border px-3 py-1 text-sm font-medium whitespace-nowrap print:px-1.5 print:py-0.5 print:text-xs">
       {text}
     </span>
   )
