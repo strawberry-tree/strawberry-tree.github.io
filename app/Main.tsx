149993ecd76d2d14bd6c17ec9dfb46eac3493fde
송상록 <drinkingstraw33@gmail.com>
2026-01-18 02:40:05 +0900

    add: 링크드인

--- app/Main.tsx
+++ app/Main.tsx
@@ -89,7 +89,25 @@ const NavigationSection = memo(function NavigationSection() {
           >
             <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z" />
           </svg>
-          <span className="font-medium">GitHub</span>
+          <span className="font-medium">깃허브</span>
+        </a>
+        <a
+          href={siteMetadata.linkedin}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-primary-500 dark:hover:text-primary-400 flex items-center gap-2 text-gray-600 transition-colors dark:text-gray-400"
+          aria-label="LinkedIn"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+          </svg>
+          <span className="font-medium">링크드인</span>
         </a>
         <a
           href="https://velog.io/@strawberry-tree"
@@ -106,7 +124,7 @@ const NavigationSection = memo(function NavigationSection() {
           >
             <path d="M19.5 3h-15A1.5 1.5 0 003 4.5v15A1.5 1.5 0 004.5 21h15a1.5 1.5 0 001.5-1.5v-15A1.5 1.5 0 0019.5 3zm-9 12H9V9h1.5v6zm3 0H12V9h1.5v6zm3 0H15V9h1.5v6z" />
           </svg>
-          <span className="font-medium">Velog</span>
+          <span className="font-medium">기술 블로그</span>
         </a>
         <a
           href={`mailto:${siteMetadata.email}`}
@@ -120,7 +138,7 @@ const NavigationSection = memo(function NavigationSection() {
           >
             <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
           </svg>
-          <span className="font-medium">Email</span>
+          <span className="font-medium">이메일</span>
         </a>
       </div>
     </>
